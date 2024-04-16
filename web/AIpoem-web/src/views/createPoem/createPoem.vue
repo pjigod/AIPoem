253686a8ad36a717305c973fac8bdf7dfbc0eff3
phigod <93143895+pjigod@users.noreply.github.com>
2024-04-16 20:58:20 +0800

    home bg

--- web/AIpoem-web/src/views/createPoem/createPoem.vue
+++ web/AIpoem-web/src/views/createPoem/createPoem.vue
@@ -116,15 +116,15 @@ export default {
             style: '',
             poem: '',
             tempid: 0,
-            isload:false,
-            ispoem:false,
-            iscouplet:false,
-            upper:'寿比南山不老松， 松高白鹤楼',
-            lower:'福如东海长流水，水漫金山寺'
+            isload: false,
+            ispoem: false,
+            iscouplet: false,
+            upper: '寿比南山不老松， 松高白鹤楼',
+            lower: '福如东海长流水，水漫金山寺'
 
         }
     },
-    components:{
+    components: {
         poemGradiant,
         coupletGradiant
     },
@@ -137,7 +137,7 @@ export default {
             // console.log(this.flag)
         },
         createpoem() {
-          this.isload=true
+            this.isload = true
             if (this.flag === 1) {
                 this.tag = 'poem'
                 get('/poem/create', {
@@ -155,9 +155,12 @@ export default {
                             get('/poem/get', {
                                 id: this.tempid
                             }).then(res => {
-                                this.poem = res.data.data.output
-                                this.isload=false
-                                this.ispoem=true
+                                if (res.data.data.status === 1) {
+                                    this.poem = res.data.data.output
+                                    this.isload = false
+                                    this.ispoem = true
+                                }
+
                             })
                         }, 5000)
                     })
@@ -178,10 +181,11 @@ export default {
                         }).then(res => {
                             // console.log(res)
                             // console.log(res.data.data.output)
-                            this.poem = res.data.data.output
-                            this.isload=false
-                            this.iscouplet=true
-
+                            if (res.data.data.status === 1) {
+                                this.poem = res.data.data.output
+                                this.isload = false
+                                this.iscouplet = true
+                            }
                             // console.log(this.poem)
                         })
                     }, 5000)
@@ -203,10 +207,11 @@ export default {
                         }).then(res => {
                             // console.log(res)
                             // console.log(res.data.data.output)
-                            this.poem = res.data.data.output
-                            // console.log(this.poem)
-                            this.isload=false
-                            this.ispoem=true
+                            if (res.data.data.status === 1) {
+                                this.poem = res.data.data.output
+                                this.isload = false
+                                this.ispoem = true
+                            }
 
 
                         })
@@ -229,10 +234,12 @@ export default {
                             id: this.tempid
                         }).then(res => {
                             // console.log(res)
-                           
-                            this.poem = res.data.data.output
-                            this.isload=false
-                            this.ispoem=true
+
+                            if (res.data.data.status === 1) {
+                                this.poem = res.data.data.output
+                                this.isload = false
+                                this.ispoem = true
+                            }
 
 
                             // console.log(this.poem)
@@ -396,112 +403,113 @@ export default {
     align-items: center;
     justify-content: center;
 }
+
 /* .poemtocre{
     width: 315px;
     
 } */
 .dot-spinner {
-  --uib-size: 2.8rem;
-  --uib-speed: .9s;
-  --uib-color: #183153;
-  position: relative;
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  height: var(--uib-size);
-  width: var(--uib-size);
+    --uib-size: 2.8rem;
+    --uib-speed: .9s;
+    --uib-color: #183153;
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    height: var(--uib-size);
+    width: var(--uib-size);
 }
 
 .dot-spinner__dot {
-  position: absolute;
-  top: 0;
-  left: 0;
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  height: 100%;
-  width: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    height: 100%;
+    width: 100%;
 }
 
 .dot-spinner__dot::before {
-  content: '';
-  height: 20%;
-  width: 20%;
-  border-radius: 50%;
-  background-color: var(--uib-color);
-  transform: scale(0);
-  opacity: 0.5;
-  animation: pulse0112 calc(var(--uib-speed) * 1.111) ease-in-out infinite;
-  box-shadow: 0 0 20px rgba(18, 31, 53, 0.3);
+    content: '';
+    height: 20%;
+    width: 20%;
+    border-radius: 50%;
+    background-color: var(--uib-color);
+    transform: scale(0);
+    opacity: 0.5;
+    animation: pulse0112 calc(var(--uib-speed) * 1.111) ease-in-out infinite;
+    box-shadow: 0 0 20px rgba(18, 31, 53, 0.3);
 }
 
 .dot-spinner__dot:nth-child(2) {
-  transform: rotate(45deg);
+    transform: rotate(45deg);
 }
 
 .dot-spinner__dot:nth-child(2)::before {
-  animation-delay: calc(var(--uib-speed) * -0.875);
+    animation-delay: calc(var(--uib-speed) * -0.875);
 }
 
 .dot-spinner__dot:nth-child(3) {
-  transform: rotate(90deg);
+    transform: rotate(90deg);
 }
 
 .dot-spinner__dot:nth-child(3)::before {
-  animation-delay: calc(var(--uib-speed) * -0.75);
+    animation-delay: calc(var(--uib-speed) * -0.75);
 }
 
 .dot-spinner__dot:nth-child(4) {
-  transform: rotate(135deg);
+    transform: rotate(135deg);
 }
 
 .dot-spinner__dot:nth-child(4)::before {
-  animation-delay: calc(var(--uib-speed) * -0.625);
+    animation-delay: calc(var(--uib-speed) * -0.625);
 }
 
 .dot-spinner__dot:nth-child(5) {
-  transform: rotate(180deg);
+    transform: rotate(180deg);
 }
 
 .dot-spinner__dot:nth-child(5)::before {
-  animation-delay: calc(var(--uib-speed) * -0.5);
+    animation-delay: calc(var(--uib-speed) * -0.5);
 }
 
 .dot-spinner__dot:nth-child(6) {
-  transform: rotate(225deg);
+    transform: rotate(225deg);
 }
 
 .dot-spinner__dot:nth-child(6)::before {
-  animation-delay: calc(var(--uib-speed) * -0.375);
+    animation-delay: calc(var(--uib-speed) * -0.375);
 }
 
 .dot-spinner__dot:nth-child(7) {
-  transform: rotate(270deg);
+    transform: rotate(270deg);
 }
 
 .dot-spinner__dot:nth-child(7)::before {
-  animation-delay: calc(var(--uib-speed) * -0.25);
+    animation-delay: calc(var(--uib-speed) * -0.25);
 }
 
 .dot-spinner__dot:nth-child(8) {
-  transform: rotate(315deg);
+    transform: rotate(315deg);
 }
 
 .dot-spinner__dot:nth-child(8)::before {
-  animation-delay: calc(var(--uib-speed) * -0.125);
+    animation-delay: calc(var(--uib-speed) * -0.125);
 }
 
 @keyframes pulse0112 {
-  0%,
-  100% {
-    transform: scale(0);
-    opacity: 0.5;
-  }
 
-  50% {
-    transform: scale(1);
-    opacity: 1;
-  }
+    0%,
+    100% {
+        transform: scale(0);
+        opacity: 0.5;
+    }
+
+    50% {
+        transform: scale(1);
+        opacity: 1;
+    }
 }
-
 </style>
--- web/AIpoem-web/src/views/home/home.vue
+++ web/AIpoem-web/src/views/home/home.vue
@@ -1,23 +1,68 @@
 <template>
-<Menu></Menu>
-<div class="container2"> 
-</div>
+    <div class="hcontainer">
+        <div class="section">
+            <Menu></Menu>
+        </div>
+        <div class="section">
+
+        </div>
+        <div class="section">
+
+        </div>
+        <div class="section">
+
+        </div>
+    </div>
 </template>
 <script>
 
-    export default {
-        methods:{
-            navTo(url){
-                this.$router.push(url)
-            }
-        },
-        components:{
-            
+export default {
+    methods: {
+        navTo(url) {
+            this.$router.push(url)
         }
+    },
+    components: {
+
     }
+}
 </script>
 <style>
+.hcontainer {
+    position: relative;
+    width: 100%;
+    height: 100vh;
+}
 
+.hcontainer .section {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    background-blend-mode: multiply;
+}
 
+.hcontainer .section:nth-child(1) {
+    background: url(../../static/img/background.jpg);
+    background-size: 100% 100%;
+    background-attachment: fixed;
+}
 
+.hcontainer .section:nth-child(2) {
+    background: url(../../static/img/bg1.jpg);
+    background-size: cover;
+    background-attachment: fixed;
+}
+
+.hcontainer .section:nth-child(3) {
+    background: url(../../static/img/bg2.jpg);
+    background-size: cover;
+    background-attachment: fixed;
+}
+
+.hcontainer .section:nth-child(4) {
+    background: url(../../static/img/bg3.jpg);
+    background-size: cover;
+    background-attachment: fixed;
+    z-index: -1;
+}
 </style>
