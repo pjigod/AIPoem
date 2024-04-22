bfde4ebe238a1f57007eeabcd9a14dca1b2c45b8
anqing <3396775114@qq.com>
2024-04-22 08:01:25 +0800

    首页样式调整以及跳转bug修复

--- web/AIpoem-web/src/views/home/home.vue
+++ web/AIpoem-web/src/views/home/home.vue
@@ -47,13 +47,13 @@
       <div v-if="flag===2" class="section-two fading-text">
         <div class="section-two-top-left">
           <p class="title2">唐诗介绍</p>
-          <p class="paragraph1"></p>
+          <p class="paragraph1">唐诗是中华民族珍贵的文化遗产之一，是中华文化宝库中的一颗明珠，同时也对世界上许多国家的文化发展产生了很大影响，对于后人研究唐代的政治、民情、风俗、文化等都有重要的参考意义。</p>
         </div>
-        <a href="#" class="section-two-top-right" @mouseover="showtooltip1" @mouseleave="hidetooltip1">唐诗的形式</a>
+        <a href="####" class="section-two-top-right" @mouseover="showtooltip1" @mouseleave="hidetooltip1">唐诗的形式</a>
         <div class="poem-form fading-text" v-if="tooltip[0]">唐诗的形式有多种，主要有五言和七言两种。近体诗也有两种，一种叫做绝句，一种叫做律诗。绝句和律诗又各有五言和七言之不同。所以唐诗的基本形式基本上有这样六种：五言古体诗，七言古体诗，五言绝句，七言绝句，五言律诗，七言律诗。古体诗对音韵格律的要求比较宽：一首之中，句数可多可少，篇章可长可短，韵脚可以转换。近体诗对音韵格律的要求比较严：一首诗的句数有限定，即绝句四句，律诗八句，每句诗中用字的平仄声，有一定的规律，韵脚不能转换；律诗还要求中间四句成为对仗。古体诗的风格是前代流传下来的，所以又叫古风。近体诗有严整的格律，所以有人又称它为格律诗。</div>
-        <a href="#" class="section-two-bottom-left" @mouseover="showtooltip2" @mouseleave="hidetooltip2">唐诗的风格</a>
+        <a href="####" class="section-two-bottom-left" @mouseover="showtooltip2" @mouseleave="hidetooltip2">唐诗的风格</a>
         <div class="poem-style fading-text" v-if="tooltip[1]">唐诗的风格丰富多彩，它继承了汉魏民歌、乐府传统，并且大大发展了歌行体的样式；不仅继承了前代的五、七言古诗，并且发展为叙事言情的长篇巨制；不仅扩展了五言、七言形式的运用，还创造了风格特别优美整齐的近体诗。近体诗是当时的新体诗，它的创造和成熟，是唐代诗歌发展史上的一件大事。它把中国古曲诗歌的音节和谐、文字精炼的艺术特色，推到前所未有的高度，为古代抒情诗找到一个最典型的形式，还特别为人民所喜闻乐见。但是近体诗中的律诗，由于它有严格的格律的限制，容易使诗的内容受到束缚，不能自由创造和发挥，这是它的长处带来的一个很大的缺陷。</div>
-        <a href="#" class="section-two-bottom-right" @mouseover="showtooltip3" @mouseleave="hidetooltip3">唐诗的流派</a>
+        <a href="####" class="section-two-bottom-right" @mouseover="showtooltip3" @mouseleave="hidetooltip3">唐诗的流派</a>
         <div class="poem-stream fading-text" v-if="tooltip[2]">
           <p>主要流派有下面几种</p>
           <ul>
@@ -70,17 +70,17 @@
         <p class="title3">AI写诗原理</p>
         <p class="paragraph3">通过使用LSTM神经网络，TransFormer模型以及TensorFlow机器学习框架构建了一整个AI写诗的算法模型进行AI写诗</p>
         </div>
-        <a href="#" class="section-three-top-right" @mouseover="showtooltip4" @mouseleave="hidetooltip4">LSTM</a>
+        <a href="####" class="section-three-top-right" @mouseover="showtooltip4" @mouseleave="hidetooltip4">LSTM</a>
         <div class="LSTM fading-text" v-if="tooltip[3]">LSTM是一种递归神经网络（RNN）的变种，设计用来解决RNN中的梯度消失和梯度爆炸问题。
 LSTM具有记忆单元和门控机制，能够更有效地处理和记忆时间序列数据,
 且适用于序列建模任务，如自然语言处理中的语言建模、机器翻译等。
-<br>在此项目中，LSTM在这段代码中的作用是作为一个序列模型，接收词嵌入的序列作为输入，在训练过程中学习诗歌文本的语言模式，并在生成诗歌时根据上下文和当前的隐藏状态生成下一个词的预测。</div>
-        <a href="#" class="section-three-bottom-left" @mouseover="showtooltip5" @mouseleave="hidetooltip5">TensorFlow</a>
+<br>在此项目中，LSTM的作用是作为一个序列模型，接收词嵌入的序列作为输入，在训练过程中学习诗歌文本的语言模式，并在生成诗歌时根据上下文和当前的隐藏状态生成下一个词的预测。</div>
+        <a href="####" class="section-three-bottom-left" @mouseover="showtooltip5" @mouseleave="hidetooltip5">TensorFlow</a>
         <div class="TensorFlow fading-text" v-if="tooltip[4]">TensorFlow是由Google开发的开源机器学习框架，广泛用于构建和训练机器学习模型。
 TensorFlow支持动态计算图和静态计算图，提供了高层次的抽象接口和低层次的灵活性,
 且能够在多种硬件平台上运行，包括CPU、GPU和TPU（Tensor Processing Unit）等。TensorFlow通常应用在图像识别和处理（包括物体检测、图像分类、语义分割等）、自然语言处理（用于文本生成、命名实体识别、机器翻译等）、时间序列预测等多种问题上（例如股价预测、天气预报等）
 <br>在此项目中，TensorFlow建立、训练并保存了神经网络模型，实现了基于我们所给定文本生成整首诗的功能</div>
-        <a href="#" class="section-three-bottom-right" @mouseover="showtooltip6" @mouseleave="hidetooltip6">Transformer</a>
+        <a href="####" class="section-three-bottom-right" @mouseover="showtooltip6" @mouseleave="hidetooltip6">Transformer</a>
         <div class="Transformer fading-text" v-if="tooltip[5]">Transformer是一种基于自注意力机制的模型，用于处理序列到序列的任务，例如机器翻译和文本生成。
 Transformer取代了传统的循环神经网络（RNN）结构，在处理长距离依赖性任务上表现更好，
 并且通过多头注意力机制实现了并行计算，提高了训练速度和效率。
@@ -185,7 +185,22 @@ export default {
   width: 100%;
   height: 100vh;
 }
-
+.section-poem1{
+  font-family: "KaiTi";
+  font-size: 20px;
+}
+.section-duilian1{
+  font-family: "KaiTi";
+  font-size: 20px;
+}
+.section-style1{
+  font-family: "KaiTi";
+  font-size: 20px;
+}
+.section-head1{
+  font-family: "KaiTi";
+  font-size: 20px;
+}
 .top-home {
   width: 100%;
   clear: both;
@@ -323,7 +338,7 @@ export default {
 }
 .introduction-title{
   font-size: 50px;
-  color:rgb(122, 126, 113);;
+  color:rgb(5, 5, 5);;
   font-family: "KaiTi";
   position: absolute;
   left: 45%;
@@ -346,6 +361,12 @@ export default {
 }
 .title2{
   font-size: 40px;
+  font-family: "KaiTi";
+}
+.paragraph1{
+  font-family: "KaiTi";
+  font-size: 20px;
+  width:70%
 }
 .section-two-top-left{
   position: absolute;
@@ -358,6 +379,7 @@ export default {
   top: 0;
   right: 0;
   font-size: 30px;
+  font-family: "KaiTi";
 }
 .poem-form{
   position: absolute;
@@ -370,12 +392,14 @@ export default {
   border-radius: 10px;
   box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1); /* 添加阴影效果 */
   border: 1px solid #e1e1e1; /* 添加边框 */
+  font-family: "KaiTi";
 }
 .section-two-bottom-left{
   position: absolute;
   bottom: 0;
   left: 0;
   font-size: 30px;
+  font-family: "KaiTi";
 }
 .poem-style{
   position: absolute;
@@ -388,17 +412,19 @@ export default {
   border-radius: 10px;
   box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1); /* 添加阴影效果 */
   border: 1px solid #e1e1e1; /* 添加边框 */
+  font-family: "KaiTi";
 }
 .section-two-bottom-right{
   position: absolute;
   bottom: 0;
   right: 0;
   font-size: 30px;
+  font-family: "KaiTi";
 }
 .poem-stream{
   position: absolute;
-  top: 20%;
-  right: 20%;
+  top: 10%;
+  right: 25%;
   width: 50%;
   font-size: 25px;
   padding: 20px;
@@ -406,6 +432,7 @@ export default {
   border-radius: 10px;
   box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1); /* 添加阴影效果 */
   border: 1px solid #e1e1e1; /* 添加边框 */
+  font-family: "KaiTi";
 }
 .section-three{
   display: block;
@@ -417,10 +444,12 @@ export default {
 }
 .title3{
   font-size: 40px;
+  font-family: "KaiTi";
 }
 .paragraph3{
   width: 15%;
   font-size: 20px;
+  font-family: "KaiTi";
 }
 .section-three-top-left{
   position: absolute;
@@ -433,6 +462,7 @@ export default {
   top: 0;
   right: 0;
   font-size: 30px;
+  font-family: "KaiTi";
 }
 .LSTM{
   position: absolute;
@@ -445,12 +475,14 @@ export default {
   border-radius: 10px;
   box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1); /* 添加阴影效果 */
   border: 1px solid #e1e1e1; /* 添加边框 */
+  font-family: "KaiTi";
 }
 .section-three-bottom-left{
   position: absolute;
   bottom: 0;
   left: 0;
   font-size: 30px;
+  font-family: "KaiTi";
 }
 .TensorFlow{
   position: absolute;
@@ -463,12 +495,14 @@ export default {
   border-radius: 10px;
   box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1); /* 添加阴影效果 */
   border: 1px solid #e1e1e1; /* 添加边框 */
+  font-family: "KaiTi";
 }
 .section-three-bottom-right{
   position: absolute;
   bottom: 0;
   right: 0;
   font-size: 30px;
+  font-family: "KaiTi";
 }
 .Transformer{
   position: absolute;
@@ -481,6 +515,7 @@ export default {
   border-radius: 10px;
   box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1); /* 添加阴影效果 */
   border: 1px solid #e1e1e1; /* 添加边框 */
+  font-family: "KaiTi";
 }
 .section-four{
   display: block;
